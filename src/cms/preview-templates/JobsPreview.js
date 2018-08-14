8a7cdfc4c5df937473199edb05618299ded9850e
martinkeck <contact@martinkeck.com>
2018-08-14 17:21:19 +0200

    Add jobs

--- src/cms/preview-templates/JobsPreview.js
+++ src/cms/preview-templates/JobsPreview.js
@@ -8,10 +8,8 @@ import {JobsTemplate} from '../../templates/jobs-page'
 const JobsPreview = ({entry, widgetFor}) => (
   <JobsTemplate
     content={widgetFor('body')}
-    cover={entry.getIn(['data', 'cover'])}
     meta_title={entry.getIn(['data', 'meta_title'])}
     meta_desc={entry.getIn(['data', 'meta_description'])}
-    tags={entry.getIn(['data', 'tags'])}
     title={entry.getIn(['data', 'title'])}
     slug={entry.getIn(['data', 'slug'])}
   />
