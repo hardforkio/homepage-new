17e222d602a3a05c5df6cb421b76937cc7292c8f
martinkeck <contact@martinkeck.com>
2018-08-14 15:07:40 +0200

    Add ThemeComponent and injectGlobal

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,21 +1,20 @@
-/**
- * Created by vaibhav on 31/3/18
- */
 import React from 'react'
 import config from '../../../meta/config'
+import styled from 'styled-components'
 
-const Footer = () => {
-  return (
-    <footer className='footer'>
-      <div className='container'>
-        <div className='content has-text-centered'>
-          <p>
-            {config.copyright}
-          </p>
-        </div>
+const Footer = styled.footer`
+  background-color: ${props => props.theme.defaultBg};
+  color: ${props => props.theme.primary};
+`
+
+export default () => (
+  <Footer className='footer'>
+    <div className='container'>
+      <div className='content has-text-centered'>
+        <p>
+          {config.copyright}
+        </p>
       </div>
-    </footer>
-  )
-}
-
-export default Footer
+    </div>
+  </Footer>
+)
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,13 +1,27 @@
-/**
- * Created by vaibhav on 31/3/18
- */
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
 import NavBar from '../components/NavBar'
 import Footer from '../components/Footer'
-import './styles.sass'
 import config from '../../meta/config'
+import {ThemeProvider, injectGlobal} from 'styled-components'
+
+injectGlobal`
+  @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,800,800italic');
+  
+  body {
+    font-family: Open Sans,Helvetica,sans-serif;
+    font-weight: 400;
+    letter-spacing: .075em;
+    line-height: 1.65em;
+    margin: 0;
+  }
+`
+
+const hardforkTheme = {
+  primary: '#ffffff',
+  defaultBg: 'rgb(46, 56, 66)',
+}
 
 const TemplateWrapper = ({children}) => (
   <div>
@@ -15,9 +29,15 @@ const TemplateWrapper = ({children}) => (
       <title>{config.siteTitle}</title>
       <meta name='description' content={config.siteDescription} />
     </Helmet>
-    <NavBar />
-    <div>{children()}</div>
-    <Footer />
+    <ThemeProvider theme={hardforkTheme}>
+      <NavBar />
+    </ThemeProvider>
+    <ThemeProvider theme={hardforkTheme}>
+      {children()}
+    </ThemeProvider>
+    <ThemeProvider theme={hardforkTheme}>
+      <Footer />
+    </ThemeProvider>
   </div>
 )
 
