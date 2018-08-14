8a1690822a7e5df85e92c8dff3d69306b68ada08
martinkeck <contact@martinkeck.com>
2018-08-14 15:06:42 +0200

    Delete SASS plugin // Add styled-components plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -21,7 +21,7 @@ module.exports = {
   },
   plugins: [
     'gatsby-plugin-react-helmet',
-    'gatsby-plugin-sass',
+    `gatsby-plugin-styled-components`,
     {
       resolve: 'gatsby-source-filesystem',
       options: {
