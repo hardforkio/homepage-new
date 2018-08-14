8dff727a0e329c593dd807a8590463bbb549b0ba
martinkeck <contact@martinkeck.com>
2018-08-14 16:42:12 +0200

    Add media query plugin

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,28 @@
 import React from 'react'
 import config from '../../../meta/config'
 import styled from 'styled-components'
+import media from 'styled-media-query'
 
 const Footer = styled.footer`
   background-color: ${props => props.theme.defaultBg};
   color: ${props => props.theme.primary};
+  
+  ${media.lessThan('medium')`
+
+    /* screen width is less than 768px (medium) */
+    background: red;
+  `}
+
+  ${media.between('medium', 'large')`
+    /* screen width is between 768px (medium) and 1170px (large) */
+    background: green;
+  `}
+
+  ${media.greaterThan('large')`
+
+    /* screen width is greater than 1170px (large) */
+    background: blue;
+  `}
 `
 
 export default () => (
