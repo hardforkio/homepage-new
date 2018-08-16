cf5aefad93fa1814b6d28b25ff4f7a214877b84c
martinkeck <contact@martinkeck.com>
2018-08-16 16:06:34 +0200

    Style header and add sidebar menu

---
+++ src/components/Header/index.js
@@ -0,0 +1,128 @@
+import React from 'react'
+import styled from 'styled-components'
+import media from 'styled-media-query'
+import Link from 'gatsby-link'
+import { slide as Menu } from 'react-burger-menu'
+
+const PageHeader = styled.header` 
+  -moz-transition: background-color 0.2s ease;
+  -webkit-transition: background-color 0.2s ease;
+  -ms-transition: background-color 0.2s ease;
+  transition: background-color 0.2s ease;
+  background: #2e3842;
+  height: 3em;
+  left: 0;
+  line-height: 3em;
+  position: fixed;
+  top: 0;
+  width: 100%;
+`
+const LogoLink = styled.a`
+  font-weight: 800;
+  left: 1.25em;
+  letter-spacing: 0.225em;
+  position: absolute;
+  top: 0;
+`
+const MenuWrap = styled.div`
+  line-height: 1.65em;
+  position: fixed;
+  top: 0;
+  
+  div, nav {
+    box-sizing: border-box;
+  }
+  
+  & .bm-burger-button {
+    border-color: #ffffff;
+    height: 18px;
+    position: fixed;
+    right: 18px;
+    top: 18px;
+    width: 18px;
+    z-index: 10001;
+  }
+  
+  .bm-burger-bars {
+    background: #ffffff;
+    height: 2px !important;
+    opacity: 1 !important;
+  }
+  
+  .bm-menu {
+    background: #21b2a6;
+    color: #ffffff;
+    
+  }
+  
+  .bm-cross-button {
+    right: 12px !important;
+    height: 18px;
+    top: 10px !important;
+    width: 18px;
+  }
+  
+  .bm-cross {
+    background: #ffffff;
+    height: 23px !important;
+    width: 2px !important;
+  }
+  
+  .bm-item {
+    list-style: none;
+    margin: 0 0 2em 0;
+    padding: 0;
+    
+    li {
+      border-top: solid 1px rgba(0, 0, 0, 0.125);
+      margin: 0.5em 0 0 0;
+      padding: 0.5em 0 0 0;
+      
+      a {
+        border: 0;
+        color: inherit;
+        display: block;
+        font-size: 0.8em;
+        letter-spacing: 0.225em;
+        line-height: 3em;
+        outline: 0;
+        text-decoration: none;
+        text-transform: uppercase;
+        
+        ${media.greaterThan('medium')`
+          line-height: 1.65em;
+        `}
+      }
+      
+      &:first-child {
+        border-top: 0 !important;
+        margin-top: 0 !important;
+        padding-top: 0 !important;
+      }
+    }
+  }
+  
+  .bm-item-list {
+    padding: 3em 2em;
+  }
+`
+
+const Header = () => {
+  return (
+    <PageHeader>
+      <LogoLink>HARDFORK</LogoLink>
+      <MenuWrap>
+        <Menu right width={'20em'}>
+          <ul>
+            <li><Link to='/' title='Startseite'>Startseite</Link></li>
+            <li><Link to='/about' title='Über uns'>Über uns</Link></li>
+            <li><Link to='/jobs' title='Jobs'>Jobs</Link></li>
+            <li><Link to='/#contact' title='Kontakt'>Kontakt</Link></li>
+          </ul>
+        </Menu>
+      </MenuWrap>
+    </PageHeader>
+  )
+}
+
+export default Header
--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,14 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
-import NavBar from '../components/NavBar'
+import Header from '../components/Header'
 import Footer from '../components/Footer'
 import config from '../../meta/config'
+import styledNormalize from 'styled-normalize'
 import {ThemeProvider, injectGlobal} from 'styled-components'
 import media from 'styled-media-query'
 
 injectGlobal`
+  ${styledNormalize}
+
   @import url('https://fonts.googleapis.com/css?family=Open+Sans:400,400italic,600,600italic,800,800italic');
+  
+  * {
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+  }
   
   body {
     background: rgb(46, 56, 66);
@@ -27,6 +35,7 @@ injectGlobal`
       font-size: 13pt;
     `}
   }
+  
 `
 
 const hardforkTheme = {
@@ -41,7 +50,7 @@ const TemplateWrapper = ({children}) => (
       <meta name='description' content={config.siteDescription} />
     </Helmet>
     <ThemeProvider theme={hardforkTheme}>
-      <NavBar />
+      <Header />
     </ThemeProvider>
     <ThemeProvider theme={hardforkTheme}>
       {children()}
