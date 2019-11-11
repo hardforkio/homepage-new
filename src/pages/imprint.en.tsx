c82c625a996a6cb9234425474fa72bb7d7bee0fb
Cavan King <king@hardfork.io>
2019-11-11 09:19:10 +0100

    Use the locale context

--- src/pages/imprint.en.tsx
+++ src/pages/imprint.en.tsx
@@ -1,18 +1,20 @@
 import React, { FunctionComponent } from 'react'
 import Layout from '../components/layout'
 import { Imprint } from '../components/Imprint'
-import { NavbarStateProvider } from '../utils/hooks'
+import { NavbarStateProvider, LocaleProvider } from '../utils/hooks'
 import getImprints from '../data/imprint'
 
 const ImprintPage: FunctionComponent<{}> = () => {
   const imprint = getImprints('en')[0]
 
   return (
-    <NavbarStateProvider>
-      <Layout title={imprint.metaTitle}>
-        <Imprint imprint={imprint} />
-      </Layout>
-    </NavbarStateProvider>
+    <LocaleProvider value="en">
+      <NavbarStateProvider>
+        <Layout title={imprint.metaTitle}>
+          <Imprint />
+        </Layout>
+      </NavbarStateProvider>
+    </LocaleProvider>
   )
 }
 
