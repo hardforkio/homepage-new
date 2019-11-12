8714c272f209972d28850500ae693d9f78debcdf
kiliw <kilocheat@gmail.com>
2019-11-12 11:23:37 +0100

    Restructure the hero section component.

--- src/components/BlurryBackground/index.tsx
+++ src/components/BlurryBackground/index.tsx
@@ -3,14 +3,19 @@ import cn from 'classnames'
 import { blurryBackgroundBanner } from '../../scss/global.module.scss'
 import styles from './Background.module.scss'
 
-export const BlurryBackground: FunctionComponent<{ show: boolean }> = ({
-  show,
-}) => (
-  <div
-    className={cn(
-      blurryBackgroundBanner,
-      styles.introBackground,
-      show ? 'show' : '',
-    )}
-  />
+export const WithBlurryBackground: FunctionComponent<{
+  show: boolean
+  className: string
+}> = ({ show, children, className }) => (
+  <div className={className}>
+    <div
+      className={cn(
+        blurryBackgroundBanner,
+        styles.introBackground,
+        show ? 'show' : '',
+        'position-absolute',
+      )}
+    />
+    {children}
+  </div>
 )
