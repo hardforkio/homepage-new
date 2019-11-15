b1c3dc3b54f6eee1260386ec961f50ea8a78a0e5
kiliw <kilocheat@gmail.com>
2019-11-15 09:53:02 +0100

    Add the correct preview of the faq page content to netlify cms preview.

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -1,4 +1,7 @@
 import CMS from 'netlify-cms-app'
 import { ImprintPreview } from './preview-templates/ImprintPreview'
+import { FAQPreview } from './preview-templates/FAQPreview'
+import '../scss/main.scss'
 
 CMS.registerPreviewTemplate('imprint', ImprintPreview)
+CMS.registerPreviewTemplate('faq', FAQPreview)
