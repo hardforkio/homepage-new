4a02c543cddcd95509e2a9dfb2c186eeb9d2be94
Cavan King <king@hardfork.io>
2019-11-21 11:18:54 +0100

    Refactor CMS preview templates to use new i18n functions

--- src/cms/preview-templates/HomePreview.tsx
+++ src/cms/preview-templates/HomePreview.tsx
@@ -1,9 +1,9 @@
 import React, { FunctionComponent } from 'react'
 import { PreviewProps, getJSON } from './helpers'
-import { getTranslation } from '../../data/i18n'
 import { HomePageTemplate } from '../../components/Home'
 import { HomeData, HomeDataOnDisk } from '../../data/home'
 import { createPreview } from './Preview'
+import { filterByLocale } from '../../data/i18n'
 
 const Preview = createPreview<HomeDataOnDisk, HomeData>()
 
@@ -11,6 +11,6 @@ export const HomePreview: FunctionComponent<PreviewProps> = ({ entry }) => (
   <Preview
     Component={HomePageTemplate}
     data={getJSON(entry)}
-    translator={getTranslation}
+    translator={filterByLocale}
   />
 )
