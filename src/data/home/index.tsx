624e33bb94bbd0f25add678b815e5374ada75ebe
kiliw <kilocheat@gmail.com>
2019-11-29 13:00:07 +0100

    Add new content type for technologies and use a relation in the projects.

--- src/data/home/index.tsx
+++ src/data/home/index.tsx
@@ -3,12 +3,33 @@ import data from './home.json'
 import { TranslationCollection, Locale, filterByLocale } from '../i18n'
 import { useLocale } from '../../utils/hooks'
 import { Head } from '../../components/Head'
+import { StringifiedTechnologies, Technology } from '../technology'
+import { convertTechnologies } from '../helpers'
 
 interface Offering {
   headline: string
   image: string
   text: string
 }
+
+interface TranslatedProjectProps {
+  slug: string
+  client: string
+  clientLink: string
+  reference: string
+  image: string
+  product: string
+  application: string
+  responsibilities: string
+}
+
+export type ProjectProps = {
+  usedTechnologies: Technology[]
+} & TranslatedProjectProps
+
+type ProjectPropsOnDisk = {
+  usedTechnologies: StringifiedTechnologies // Apparently the relation widget returns a list of stringified Technology objects. They are parsed later in the component whre they are used.
+} & TranslationCollection<TranslatedProjectProps>
 
 export interface HomeData {
   heroHeadline: string
@@ -20,7 +41,9 @@ export interface HomeData {
   moreLinkText: string
   emailButton: string
   contactEmail: string
+  projectsHeadline?: string
   head: Head
+  projects: ProjectProps[]
 }
 
 export type HomeDataOnDisk = {
@@ -28,6 +51,7 @@ export type HomeDataOnDisk = {
   offerings: TranslationCollection<Offering>[]
   contactEmail: string
   head: TranslationCollection<Head>
+  projects: ProjectPropsOnDisk[]
 } & TranslationCollection<{
   heroHeadline: string
   heroSubheadline: string
@@ -36,9 +60,13 @@ export type HomeDataOnDisk = {
   contactButtonText: string
   moreLinkText: string
   emailButton: string
+  projectsHeadline?: string
 }>
 
 const home: HomeDataOnDisk = data
-export const getHome = (locale: Locale): HomeData =>
-  filterByLocale(locale)(home)
+export const getHome = (locale: Locale): HomeData => {
+  const filteredHome = filterByLocale(locale)(home)
+  return convertTechnologies(filteredHome)
+}
+
 export const useHome: () => HomeData = R.pipe(useLocale, getHome)
