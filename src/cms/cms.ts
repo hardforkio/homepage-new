4e524cbbf45ec29418b25f9f29c43cab85691ff1
EmCeeEs <ab22uhrnachtunruhe@gmail.com>
2019-11-29 09:46:58 +0100

    Use LocalizedStringWidget in Footer.

--- src/cms/cms.ts
+++ src/cms/cms.ts
@@ -5,6 +5,9 @@ import { FooterPreview } from './preview-templates/FooterPreview'
 import '../scss/main.scss'
 import { HomePreview } from './preview-templates/HomePreview'
 import { NavbarPreview } from './preview-templates/NavbarPreview'
+import { createLocalizedStringWidget } from './widgets/LocalizedString'
+
+const LocalizedStringControl = createLocalizedStringWidget(['en', 'de'])
 
 const config = {
   backend: {
@@ -15,8 +18,11 @@ const config = {
 }
 
 init({ config })
+
 CMS.registerPreviewTemplate('imprint', ImprintPreview)
 CMS.registerPreviewTemplate('faq', FAQPreview)
 CMS.registerPreviewTemplate('home', HomePreview)
 CMS.registerPreviewTemplate('footer', FooterPreview)
 CMS.registerPreviewTemplate('navbar', NavbarPreview)
+
+CMS.registerWidget('localizedString', LocalizedStringControl)
