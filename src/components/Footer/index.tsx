3ccc44ef6f5a4c2a01b295096413ca199c6976ac
kiliw <kilocheat@gmail.com>
2019-12-04 10:41:05 +0100

    Fix footer imports

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,8 +3,9 @@ import { FooterComponent } from './component'
 import { FunctionComponent } from 'react'
 import { Link } from 'gatsby'
 import { SafeExternalLink } from '../Link'
-import { useFooter, FooterData } from '../../data/footer'
+import { FooterData } from '../../data/footer'
 import { usePathPrefix } from '../../utils/hooks'
+import { useFooter } from '../../hooks/data'
 
 export const Footer: FunctionComponent = () => {
   const footerData: FooterData = useFooter()
