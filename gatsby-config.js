c96e74090b8bf56ae3a123c8ea4851ba932f929f
Levin Keller <post@levinkeller.de>
2019-12-16 15:00:54 +0100

    Overwrite referrer policy for form post

--- gatsby-config.js
+++ gatsby-config.js
@@ -73,7 +73,7 @@ module.exports = {
       resolve: 'gatsby-plugin-netlify',
       options: {
         headers: {
-          '/*': ['referrer-policy: strict-origin'],
+          '/*': ['Referrer-Policy: strict-origin'],
         },
       },
     },
