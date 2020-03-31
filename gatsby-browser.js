ee0e96fd198cc33fafca8da0e2d7adc7c641260e
Levin Keller <post@levinkeller.de>
2020-03-31 09:53:24 +0200

    Fix endless redirect.

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -7,11 +7,3 @@ new SmoothScroll(LINK_SELECTOR, {
   speed: 1500,
   speedAsDuration: true,
 })
-
-// When one would go to /de/#contact from external, one would stay at the top of the page.
-// onInitialClientRender is called on the initial render of the gatsby page and tiggers a
-// navigation to the current location which scrolls to the desired section.
-
-export const onInitialClientRender = () => {
-  window.___navigate(window.location.href)
-}
