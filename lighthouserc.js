3496f1f1dc8518802023984646e289777ec4e153
Jakob J. Kolb <kolb@hardfork.io>
2020-07-17 12:24:50 +0200

    Fix lighthouse config to check production build.

--- lighthouserc.js
+++ lighthouserc.js
@@ -1,5 +1,12 @@
 module.exports = {
   ci: {
+    collect: {
+      url: ['http://localhost/de/index.html'],
+      staticDistDir: 'public',
+    },
+    assert: {
+      preset: 'lighthouse:recommended',
+    },
     upload: {
       target: 'temporary-public-storage',
     },
